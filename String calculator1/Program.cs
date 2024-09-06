89950db676cb1a6da055224eb277f186f73fed05
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-06 10:09:50 +0200

    Add files via upload

---
+++ String calculator1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Skriv din mening här: ");
+
+string input = Console.ReadLine();
+
+int counter = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+	if (char.IsLetter(input[i]))
+	{
+		counter++;
+	}
+    
+}
+
+Console.WriteLine("Antalet bokstäver: " + counter);
